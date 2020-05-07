de74fc9ed11f8ebdf49f5d857c02ccd4387a1692
Rostic <birnaz98@mail.ru>
2020-05-07 21:18:02 +0300

    exercitiul 1

---
+++ laborator2/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.backgroundColor = 'violet'
+    const newDiv = document.createElement('div')
+    newDiv.id = 'text'
+    newDiv.innerText = 'START'
+    document.body.appendChild(newDiv)
+   
+});

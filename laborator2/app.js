880e2227956fafcc08248ca05f305c105deccebb
Rostic <birnaz98@mail.ru>
2020-05-07 21:25:29 +0300

    ex 3

--- laborator2/app.js
+++ laborator2/app.js
@@ -6,6 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(newDiv)
      setTimeout(() => {
      	document.body.style.backgroundColor = 'green'
-    }, 3000);
+    }, 1000);
    
+
+     setTimeout(() => {
+     	 let list = document.createElement('ol')
+   		 const numbers = 15
+    	let count = 0
+    	setInterval(() => {
+        	if (count < numbers) {
+            	count++
+            	let li = document.createElement('li')
+            	li.innerText = count
+            	list.appendChild(li)
+            	document.body.appendChild(list)
+        	}
+    	}, 1000)
+    }, 3000);
+
 });
